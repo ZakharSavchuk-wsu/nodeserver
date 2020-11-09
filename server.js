06c8ee3f0f605f4ffabb3f04609373fa85ad4d87
Zakhar_Savchuk <savchukzahar33@gmail.com>
2020-11-09 17:02:08 +0100

    first changes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ module.exports = router;
 // }
 app.get('/', (req, res) => res.send('Welcome to HomaPage'))
 
-// Set up a port
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
