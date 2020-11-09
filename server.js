d134dbd26f143104991656244216015e2df3180e
Zakhar_Savchuk <savchukzahar33@gmail.com>
2020-11-09 16:42:27 +0100

    first changest

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ module.exports = router;
 // Combine react and node js servers while deploying( YOU MIGHT HAVE ALREADY DONE THIS BEFORE
 // What you need to do is make the build directory on the heroku, which will contain the index.html of your react app and then point the HTTP request to the client/build directory
 
-if (process.env.NODE_ENV === 'production') {
-	// Set a static folder
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set a static folder
+// 	app.use(express.static('client/build'));
+// 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
-}
+// }
+app.get('/', (req, res) => res.send('Welcome to HomaPage'))
 
 // Set up a port
 const port = process.env.PORT || 5000;
